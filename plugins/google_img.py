7c75e647eebbee72fc4edd50f7856f6fec8713bf
StuartBasunia <68786421+StuartBasunia@users.noreply.github.com>
2020-12-02 00:14:07 +0600

    Update google_img.py

--- plugins/google_img.py
+++ plugins/google_img.py
@@ -50,7 +50,7 @@ async def google_img(message: Message):
         "q": query,
         "num": 7,
         "safe": "off",
-        "fileType": "jpg",
+        "fileType": "jpg,png,webp",
         "imgType": "photo",
         "imgSize": "HUGE",
     }
